12cff9349fd4c33f83012c30b19e30b0c04cf124
igawolanin <igatwolanin@gmail.com>
2025-03-11 21:15:11 +0100

    Initial Commit

---
+++ aizo1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  aizo1
+//
+//  Created by Iga Wolanin on 11/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
